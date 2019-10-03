b750debd64c4876ffcd90f584b8a934ca64c5bb9
iamabhinav-am <56130087+iamabhinav-am@users.noreply.github.com>
2019-10-03 22:54:01 +0530

    Update AppendAndDelete.cpp

--- AppendAndDelete.cpp
+++ AppendAndDelete.cpp
@@ -36,8 +36,7 @@ int main(){
         cout<<"Yes";
         return 0;
     }
-    for(i=0,j=0;i<s.size()&&j<t.size();i++,j++)
-    {
+    for(i=0,j=0;i<s.size()&&j<t.size();i++,j++){
         if(s[i]!=t[j])
 		 	break;
    }
